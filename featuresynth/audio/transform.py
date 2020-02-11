3fd349b7d40680f39c2533a15c5662c8236091ba
John Vinyard <john.vinyard@gmail.com>
2020-02-11 15:00:04 -0600

    Working implementation of FFT-based resampling, frequency decomposition and recomposition in PyTorch

--- featuresynth/audio/transform.py
+++ featuresynth/audio/transform.py
@@ -1,5 +1,6 @@
 import zounds
 import numpy as np
+import torch
 
 
 def overlap_add(x):
@@ -44,3 +45,78 @@ def imdct(coeffs):
     recon = zounds.spectral.functional.imdct(coeffs)
     recon = overlap_add(recon)
     return recon
+
+
+def fft_frequency_decompose(x, min_size):
+    coeffs = torch.rfft(input=x, signal_ndim=1, normalized=True)
+
+    def make_mask(size, start, stop):
+        mask = torch.zeros(size)
+        mask[start:stop] = 1
+        return mask[None, None, :, None]
+
+    output = {}
+
+    current_size = min_size
+
+    while current_size <= x.shape[-1]:
+        sl = coeffs[:, :, :current_size // 2 + 1, :]
+        if current_size > min_size:
+            mask = make_mask(
+                sl.shape[2], current_size // 4,
+                current_size // 2 + 1)
+            sl = sl * mask
+        recon = torch.irfft(
+            input=sl,
+            signal_ndim=1,
+            normalized=True,
+            signal_sizes=(current_size,))
+        output[recon.shape[-1]] = recon
+        current_size *= 2
+
+    return output
+
+
+def fft_resample(x, desired_size):
+    batch, channels, time = x.shape
+    coeffs = torch.rfft(input=x, signal_ndim=1, normalized=True)
+
+    new_coeffs_size = desired_size // 2 + 1
+    new_coeffs = torch.zeros(batch, channels, new_coeffs_size, 2)
+    new_coeffs[:, :, :coeffs.shape[2], :] = coeffs
+
+    samples = torch.irfft(
+        input=new_coeffs,
+        signal_ndim=1,
+        normalized=True,
+        signal_sizes=(desired_size,))
+    return samples
+
+
+def fft_frequency_recompose(d, desired_size):
+    bands = []
+    for band in d.values():
+        bands.append(fft_resample(band, desired_size))
+    return sum(bands)
+
+if __name__ == '__main__':
+    app = zounds.ZoundsApp(globals=globals(), locals=locals())
+    app.start_in_thread(9999)
+
+    sr = zounds.SR11025()
+    synth = zounds.NoiseSynthesizer(sr)
+    noise = synth.synthesize(sr.frequency * 16385)
+    signal = torch.from_numpy(noise).view(1, 1, 16384).float()
+
+    bands = fft_frequency_decompose(signal, 512)
+    recon = {}
+
+    for k, v in bands.items():
+        print(k, v.shape)
+        recon[k] = zounds.AudioSamples(
+            fft_resample(v, 16384).data.cpu().numpy().squeeze(), sr)
+
+    r = fft_frequency_recompose(bands, 16384)
+    r = zounds.AudioSamples(r.data.cpu().numpy().squeeze(), sr)
+
+    input('waiting...')
